d03aa96fa406befd56023b067e44ab6f8d5ecded
shnhrrsn <shnhrrsn@gmail.com>
2019-12-28 12:14:45 -0500

    Fixed crash caused by cleanupAccessoriesIds becoming unset

--- platform/index.js
+++ platform/index.js
@@ -265,7 +265,7 @@ class Platform {
       this.accessories.delete(id);
     });
 
-    delete this.cleanupAccessoriesIds;
+    this.cleanupAccessoriesIds.clear();
 
     if (!accessories.length) {
       return
